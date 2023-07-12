f3f8c0d7c8ed184500bc979777eb880cd36142c2
alexancast <37774672+alexancast@users.noreply.github.com>
2023-07-12 16:41:24 +0200

    Basic draught following player

--- HPMMO/Assets/Environment/Scripts/TerrainController.cs
+++ HPMMO/Assets/Environment/Scripts/TerrainController.cs
@@ -9,30 +9,15 @@ public class TerrainController : MonoBehaviour
 
     [Header("Values")]
     [SerializeField] private Color livingColor, deadColor;
-    [SerializeField] private LayerMask layerMask;
-    [SerializeField, Range(0, 5)] private int steps = 1;
-    [SerializeField, Tooltip("The influence of the distance on draught.")] private float distanceEffect = 1;
-    public float power = 1;
-    public int width = 100;
+    [SerializeField] private int textureSize;
 
-    private MeshFilter meshFilter;
-    private Mesh mesh;
-    private Color[] colors;
+    private Renderer renderer;
+
+
 
     private void Start()
     {
-        meshFilter = GetComponent<MeshFilter>();
-        mesh = meshFilter.mesh;
-
-        colors = new Color[mesh.vertices.Length];
-
-        for (int i = 0; i < colors.Length; i++)
-        {
-            colors[i] = Color.green;
-        }
-
-
-        mesh.colors = colors;
+        renderer = GetComponent<Renderer>();
 
     }
 
@@ -41,7 +26,7 @@ public class TerrainController : MonoBehaviour
 
         if (context.started)
         {
-            StartCoroutine(SourceEnergy(10));
+            StartCoroutine(DrainGround(10));
         }
         else if (context.canceled)
         {
@@ -50,148 +35,33 @@ public class TerrainController : MonoBehaviour
     }
 
 
-    public IEnumerator SourceEnergy(float castTime)
-    {
-
-        Vector2 pos = new Vector2(playerTransform.position.x, playerTransform.position.z);
-
-        int triIndex = RaycastTri(pos);
-        int[] verts = GetTriangleIndices(triIndex, steps);
+    public IEnumerator DrainGround(float castTime) {
 
         float elapsedTime = 0;
-        Color[] startColors = colors;
 
         while (elapsedTime < castTime)
         {
 
-            foreach (var item in verts)
-            {
-                float dist = Vector3.Distance(playerTransform.position, mesh.vertices[item]);
-                dist = Mathf.Pow(1/dist, distanceEffect);
-                colors[item] = Color.Lerp(startColors[item], deadColor, (elapsedTime / castTime) * dist);
-
-            }
-            
-            mesh.colors = colors;
+            renderer.material.SetFloat("_Radius", (elapsedTime / castTime) * 10);
 
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
-
     }
 
-    public int RaycastTri(Vector2 pos)
+
+
+
+    public void Update()
     {
 
-        RaycastHit hit;
-
-        if (Physics.Raycast(new Vector3(pos.x, 100, pos.y), Vector3.down, out hit, Mathf.Infinity, layerMask))
-        {
-            return hit.triangleIndex;
-        }
-
-        return 0;
-
+        Vector3 pos = new Vector3(playerTransform.position.x + 50, 0, playerTransform.position.z + 50);
+        renderer.material.SetVector("_PlayerPos", pos);
     }
 
-    public int[] GetTriangleIndices(int triangleIndex, int steps)
-    {
-        int[] triangles = mesh.triangles;
 
-        // Check if the triangle index is within range
-        if (triangleIndex >= 0 && triangleIndex < triangles.Length / 3)
-        {
-            int vertexIndex = triangleIndex * 3;
-            int[] triangleIndices = new int[3];
 
-            triangleIndices[0] = triangles[vertexIndex];
-            triangleIndices[1] = triangles[vertexIndex + 1];
-            triangleIndices[2] = triangles[vertexIndex + 2];
 
-            if (steps > 0)
-            {
-                for (int i = 0; i < steps; i++)
-                {
-                    int[] expandedIndices = new int[triangleIndices.Length * 4];
-
-                    for (int j = 0; j < triangleIndices.Length; j++)
-                    {
-                        int currentVertexIndex = triangleIndices[j];
-
-                        int[] adjacentIndices = GetAdjacentVertices(currentVertexIndex);
-                        for (int k = 0; k < 4; k++)
-                        {
-                            expandedIndices[j * 4 + k] = adjacentIndices[k];
-                        }
-                    }
-
-                    triangleIndices = expandedIndices;
-                }
-            }
-
-            return triangleIndices;
-        }
-        else
-        {
-            Debug.LogError("Invalid triangle index!");
-            return null;
-        }
-    }
-
-    private int[] GetAdjacentVertices(int vertexIndex)
-    {
-        int[] adjacentIndices = new int[4];
-
-        int row = vertexIndex / width;
-        int column = vertexIndex % width;
-
-        adjacentIndices[0] = vertexIndex + 1; // Right
-        adjacentIndices[1] = vertexIndex - 1; // Left
-        adjacentIndices[2] = vertexIndex + width; // Up
-        adjacentIndices[3] = vertexIndex - width; // Down
-
-        // Check if the right vertex is within the same row
-        if ((row * width + (column + 1)) / width == row)
-        {
-            adjacentIndices[0] = row * width + (column + 1);
-        }
-        else
-        {
-            adjacentIndices[0] = vertexIndex;
-        }
-
-        // Check if the left vertex is within the same row
-        if ((row * width + (column - 1)) / width == row)
-        {
-            adjacentIndices[1] = row * width + (column - 1);
-        }
-        else
-        {
-            adjacentIndices[1] = vertexIndex;
-        }
-
-        // Check if the top vertex is within the grid
-        if (vertexIndex + width < mesh.vertexCount)
-        {
-            adjacentIndices[2] = vertexIndex + width;
-        }
-        else
-        {
-            adjacentIndices[2] = vertexIndex;
-        }
-
-        // Check if the bottom vertex is within the grid
-        if (vertexIndex - width >= 0)
-        {
-            adjacentIndices[3] = vertexIndex - width;
-        }
-        else
-        {
-            adjacentIndices[3] = vertexIndex;
-        }
-
-        return adjacentIndices;
-    }
 
 }
---
+++ HPMMO/Assets/Environment/Shaders/Terrain.shader
@@ -0,0 +1,98 @@
+Shader "Custom/Terrain"
+{
+    Properties
+    {
+        _PlayerPos ("Player Position", Vector) = (0,0,0)
+        _GrassColor ("Grass Color", Color) = (1,1,1,1)
+        _MountainColor ("Mountain Color", Color) = (1,1,1,1)
+        _BlendSharpness ("Blend Sharpness", Range(0, 1)) = 0.1
+        _TerrainHeight("Terrain Height", float) = 10
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+        _Radius ("Radius", Range(0, 10)) = 0.5
+        _SmoothingRadius ("Smoothing radius", Range(0, 5)) = 0.5
+
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+            float3 worldNormal;
+            float3 worldPos;
+        };
+
+        half _Glossiness;
+        half _Metallic;
+        fixed4 _GrassColor;
+        fixed4 _MountainColor;
+        float _BlendSharpness;
+        float _TerrainHeight;
+        float3 _PlayerPos;
+        float _Radius;
+        float _SmoothingRadius;
+
+
+        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+        // #pragma instancing_options assumeuniformscaling
+        UNITY_INSTANCING_BUFFER_START(Props)
+            // put more per-instance properties here
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+
+        float invLerp(float a, float b, float value){
+
+            return (value - a) / (b - a);
+        }
+
+        float remap(float iMin, float iMax, float oMin, float oMax, float value){
+
+            float t = invLerp(iMin, iMax, value);
+            return lerp(oMin, oMax, t);
+        }
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            /// Beräkna normalens vinkel i förhållande till uppåtriktningen (0, 1, 0)
+            float angle = dot(IN.worldNormal, float3(0, 1, 0));
+
+            // Ange tröskelvinkeln (i grader) där "_MountainColor" ska användas
+            float thresholdAngle = 0.5;
+
+            // Jämför normalvinkeln med tröskelvärdet och välj färgen baserat på resultatet
+            float blendFactor = smoothstep(0, _BlendSharpness, abs(angle - cos(radians(thresholdAngle))));
+            float3 angleCol = lerp(_GrassColor.rgb, _MountainColor.rgb, blendFactor);
+            
+
+            float3 heightCol = lerp(_MountainColor.rgb, _GrassColor.rgb, IN.worldPos.y/_TerrainHeight);
+
+
+            float dist = distance(IN.uv_MainTex, _PlayerPos.xz);
+            float alpha = 1 - smoothstep(_Radius - _SmoothingRadius, _Radius + _SmoothingRadius, dist);
+
+            dist = clamp(dist, 0, _Radius);
+
+            float3 col = lerp(heightCol, _MountainColor, blendFactor);
+
+            col = lerp(col, _MountainColor, alpha);
+            
+            o.Albedo = col;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
